6261171ccfdf33a2cc7e1e8c96cf54651836afe7
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-04-06 20:46:13 +0200

    Setup

---
+++ OdeToFood/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(privider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. 
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+
+            // .Run is a terminal middleware so no other middleware after this will get chained
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
+//ASP.NET Core 1.0 Fundamentals
+//aspdotnet-core-1-0-fundamentals
+//https://app.pluralsight.com/player?course=aspdotnet-core-1-0-fundamentals&author=scott-allen&name=aspdotnet-core-1-0-fundamentals-m2&clip=3&mode=live
