82f25d54fe6fc21c99265eeb5df9e3ef24905682
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2017-04-07 09:20:44 +0200

    commi

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,7 +31,7 @@ namespace OdeToFood
         }
 
         // This method gets called by the runtime. 
-        // Use this method to configure the HTTP request pipeline.
+        // Use this method to configure the HTTP request pipeline.sss
         public void Configure(
             IApplicationBuilder app, 
             IHostingEnvironment env, 
